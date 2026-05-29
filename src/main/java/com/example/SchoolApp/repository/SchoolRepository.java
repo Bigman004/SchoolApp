b7d1f744d06cca8cb23bfdb4b078de8d86a59be4
bigman004 <adedijiadetunji228@gmail.com>
2026-05-29 15:15:21 +0100

    monitoring production final fix 002

--- src/main/java/com/example/SchoolApp/repository/SchoolRepository.java
+++ src/main/java/com/example/SchoolApp/repository/SchoolRepository.java
@@ -4,5 +4,5 @@ import com.example.SchoolApp.model.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SchoolRepository extends JpaRepository<School, Long> {
-    School findbyUsername(String registrationNumber);
+    School findByUsername(String registrationNumber);
 }
--- src/main/java/com/example/SchoolApp/service/SchoolService.java
+++ src/main/java/com/example/SchoolApp/service/SchoolService.java
@@ -39,6 +39,6 @@ public class SchoolService {
     }
 
     public School getSchool(String registrationNumber) {
-        return schoolRepository.findbyUsername(registrationNumber);
+        return schoolRepository.findByUsername(registrationNumber);
     }
 }
