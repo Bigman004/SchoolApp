4c3a49903f2d7a8f45d1131a9005409feb026e21
bigman004 <adedijiadetunji228@gmail.com>
2026-06-01 11:48:59 +0100

    print result feature

--- src/main/java/com/example/SchoolApp/dto/PrintResultDto.java
+++ src/main/java/com/example/SchoolApp/dto/PrintResultDto.java
@@ -3,14 +3,16 @@ package com.example.SchoolApp.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Map;
+
 @Builder
 @Data
 public class PrintResultDto {
-    private ResultDto testResult;
-    private ResultDto examResult;
+    private Map<String, Integer> testResult;
+    private Map<String, Integer> examResult;
     private String studentFirstName;
     private String studentLastName;
     private String studentClass;
-
     private String regNumber;
+    private String schoolName;
 }
